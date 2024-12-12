fd7e69be588f4d68a1dab152a5eb2990e8ef1fa0
ElBrayanA <122376780+ElBrayanA@users.noreply.github.com>
2024-12-11 19:53:44 -0700

    Commmit test 2

--- frontend/src/routes/routes.client.jsx
+++ frontend/src/routes/routes.client.jsx
@@ -1,9 +1,5 @@
 import { ClientLayout } from "../layouts";
-<<<<<<< HEAD
 import {Home} from '../pages/client/Home'
-=======
-import {Home} from "../pages/client/Home";
->>>>>>> 03c6c8c0e73a1c9edd01d6e6b74e927c7fbcf9da
 
 const routesClient = [
   {
--- frontend/src/routes/routes.common.jsx
+++ frontend/src/routes/routes.common.jsx
@@ -1,13 +1,7 @@
 import { CommonLayout } from "../layouts";
-<<<<<<< HEAD
 import { Login } from "../pages/common";
 import { Register } from "../pages/common";
 import { Error404 } from "../pages/common";
-=======
-import { Login } from "../pages/common/Login";
-import { Register } from "../pages/common/Register";
-import { Error404 } from "../pages/common/Error404";
->>>>>>> 03c6c8c0e73a1c9edd01d6e6b74e927c7fbcf9da
 
 const routesCommon = [
   {
